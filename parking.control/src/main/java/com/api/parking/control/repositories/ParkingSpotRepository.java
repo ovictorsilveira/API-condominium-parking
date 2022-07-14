790328b6db3e82d4a08d872898bc49e9fbb34e8a
ovictorsilveira <ovictorsilveira@icloud.com>
2022-07-14 14:30:36 -0300

    release/1.0

---
+++ parking.control/src/main/java/com/api/parking/control/repositories/ParkingSpotRepository.java
@@ -0,0 +1,14 @@
+package com.api.parking.control.repositories;
+
+import com.api.parking.control.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartamentAndBlock(String apartment, String block);
+}
